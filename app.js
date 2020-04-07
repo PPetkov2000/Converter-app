e3426ff0ab64d47380dbd10fbf09f6487bd1eecb
PPetkov <ppetkov2000@abv.bg>
2020-04-08 01:54:00 +0300

    added more features

--- app.js
+++ app.js
@@ -1,149 +1,202 @@
-function converters() {
-  let result1 = decimalToBinary(20);
-  let result2 = binaryToDecimal("1111");
-  let result3 = decimalToHexadecimal(20);
-  let result4 = hexadecimalToDecimal("20");
-  let result5 = romanNumeralToDecimal("MDXXXIIV");
-  let result6 = binaryToText(
-    "01100111 01101111 01101111 01100100 00100000 01100100 01101111 01100111"
-  );
-  let result7 = textToBinary("good dog");
+const convertFrom = document.querySelector("#convertFrom");
+const convertTo = document.querySelector("#convertTo");
+const input = document.querySelector("#input");
+const convert = document.querySelector("#convert");
+const result = document.querySelector("#result");
 
-  function decimalToBinary(decimalNumber) {
-    let result = "";
+const numeralSystems = ["decimal", "hexadecimal", "binary", "roman"];
 
-    while (decimalNumber > 0) {
-      result = (decimalNumber % 2) + result;
-      decimalNumber = Math.floor(decimalNumber / 2);
-    }
+numeralSystems.forEach((sys) => {
+  generateOptions(convertFrom, sys, capitalize(sys));
+  generateOptions(convertTo, sys, capitalize(sys));
+});
 
-    return result;
-  }
+function generateOptions(select, optionValue, optionContent) {
+  const option = document.createElement("option");
 
-  function binaryToDecimal(binaryNumber) {
-    let result = 0;
-    let position = 0;
+  option.value = optionValue;
+  option.textContent = optionContent;
 
-    for (let i = binaryNumber.length - 1; i >= 0; i--) {
-      let currentDigit = Number(binaryNumber[i]);
-      if (currentDigit === 1) {
-        result += currentDigit * 2 ** position;
-      } else {
-        result = result;
-      }
-      position++;
-    }
-
-    return result;
-  }
-
-  function decimalToHexadecimal(decimalNumber) {
-    const map = {
-      0: "0",
-      1: "1",
-      2: "2",
-      3: "3",
-      4: "4",
-      5: "5",
-      6: "6",
-      7: "7",
-      8: "8",
-      9: "9",
-      10: "A",
-      11: "B",
-      12: "C",
-      13: "D",
-      14: "E",
-      15: "F"
-    };
-    let result = "";
-
-    while (decimalNumber > 0) {
-      let remainder = decimalNumber % 16;
-      result += map[remainder];
-
-      decimalNumber = Math.floor(decimalNumber / 16);
-    }
-
-    return result
-      .toString()
-      .split("")
-      .reverse()
-      .join("");
-  }
-
-  function hexadecimalToDecimal(hexadecimalNumber) {
-    const map = {
-      0: "0",
-      1: "1",
-      2: "2",
-      3: "3",
-      4: "4",
-      5: "5",
-      6: "6",
-      7: "7",
-      8: "8",
-      9: "9",
-      10: "A",
-      11: "B",
-      12: "C",
-      13: "D",
-      14: "E",
-      15: "F"
-    };
-    let result = 0;
-    let position = 0;
-
-    for (let i = hexadecimalNumber.length - 1; i >= 0; i--) {
-      let currentDigit = Number(hexadecimalNumber[i]);
-      result += map[currentDigit] * 16 ** position;
-      position++;
-    }
-
-    return result;
-  }
-
-  function romanNumeralToDecimal(romanNumber) {
-    const map = {
-      I: 1,
-      V: 5,
-      X: 10,
-      L: 50,
-      C: 100,
-      D: 500,
-      M: 1000
-    };
-
-    let result = 0;
-
-    for (let i = 0; i < romanNumber.length; i++) {
-      let romanSymbol = romanNumber[i];
-      if (!map.hasOwnProperty(romanSymbol)) {
-        return "Invalid number!";
-      }
-      result += map[romanSymbol];
-    }
-
-    return result;
-  }
-
-  function binaryToText(binaryNumber) {
-    return binaryNumber
-      .split(" ")
-      .map(binaryToDecimal)
-      .map(num => String.fromCharCode(num))
-      .join("");
-  }
-
-  function textToBinary(text) {
-    return text
-      .split("")
-      .map(symbol => symbol.charCodeAt(0))
-      .map(num => decimalToBinary(num).padStart(8, 0))
-      .join(" ");
-  }
-
-  return result4;
+  select.appendChild(option);
 }
 
-console.log(converters());
+function capitalize(text) {
+  return text.slice(0, 1).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+function calculate() {
+  let res = "";
+
+  if (convertFrom.value === "decimal") {
+    res = convertFromDecimal(input.value, convertTo.value);
+  } else if (convertFrom.value === "hexadecimal") {
+    res = convertFromHexadecimal(input.value, convertTo.value);
+  } else if (convertFrom.value === "binary") {
+    res = convertFromBinary(input.value, convertTo.value);
+  } else if (convertFrom.value === "roman") {
+    res = convertFromRoman(input.value, convertTo.value);
+  }
+
+  result.textContent = `Result: ${res}`;
+}
+
+convert.addEventListener("click", calculate);
+
+function convertFromDecimal(num, system) {
+  if (system === "roman") {
+    return convertToRoman(num);
+  }
+  const valuesMap = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+  };
+  const systemsMap = {
+    decimal: 10,
+    hexadecimal: 16,
+    binary: 2,
+  };
+
+  if (!systemsMap.hasOwnProperty(system)) {
+    return `Invalid numeric system!`;
+  }
+
+  let result = "";
+
+  while (num > 0) {
+    let remainder = num % systemsMap[system];
+    result += valuesMap[remainder];
+    num = Math.floor(num / systemsMap[system]);
+  }
+
+  return result.toString().split("").reverse().join("");
+}
+
+function convertFromBinary(num, system) {
+  num = num.toString();
+
+  if (!num.match(/^[01]+$/g)) {
+    return "Invalid input!";
+  }
+
+  let resultToDecimal = 0;
+  let position = 0;
+
+  for (let i = num.length - 1; i >= 0; i--) {
+    let currentDigit = Number(num[i]);
+    if (currentDigit === 1) {
+      resultToDecimal += currentDigit * 2 ** position;
+    }
+    position++;
+  }
+
+  return convertFromDecimal(resultToDecimal, system);
+}
+
+function convertFromHexadecimal(num, system) {
+  num = num.toString();
+
+  if (!num.match(/^[0-9A-F]+$/g)) {
+    return "Invalid input!";
+  }
+
+  const valuesMap = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    A: "10",
+    B: "11",
+    C: "12",
+    D: "13",
+    E: "14",
+    F: "15",
+  };
+
+  let resultToDecimal = 0;
+  let position = 0;
+
+  for (let i = num.length - 1; i >= 0; i--) {
+    let currentChar = num[i];
+    resultToDecimal += valuesMap[currentChar] * 16 ** position;
+    position++;
+  }
+
+  return convertFromDecimal(resultToDecimal, system);
+}
+
+function convertFromRoman(num, system) {
+  num = num.toString();
+
+  const valuesMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let resultToDecimal = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    let romanSymbol = num[i];
+    if (!valuesMap.hasOwnProperty(romanSymbol)) {
+      return "Invalid input!";
+    }
+    resultToDecimal += valuesMap[romanSymbol];
+  }
+
+  return convertFromDecimal(resultToDecimal, system);
+}
+
+function convertToRoman(num) {
+  const mapValues = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M",
+  };
+
+  let result = "";
+
+  while (num > 0) {
+    let newMap = Object.keys(mapValues)
+      .map(Number)
+      .filter((key) => key <= num);
+    let highestValue = Math.max(...newMap);
+    result += mapValues[highestValue];
+    num -= highestValue;
+  }
+
+  return result;
+}
