09f34e52dc0f193d2ff7eb70e0e70b5542d021e4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-06-22 22:29:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,36 @@
-const convertFrom = document.querySelector("#convertFrom");
-const convertTo = document.querySelector("#convertTo");
-const input = document.querySelector("#input");
-const convert = document.querySelector("#convert");
-const result = document.querySelector("#result");
+import { generateOptions } from './utils.js'
 
-const numeralSystems = ["decimal", "hexadecimal", "binary", "roman"];
+const convertFrom = document.querySelector("#convertFrom")
+const convertTo = document.querySelector("#convertTo")
+const input = document.querySelector("#input")
+const convert = document.querySelector("#convert")
+const result = document.querySelector("#result")
 
-numeralSystems.forEach((sys) => {
-  generateOptions(convertFrom, sys, capitalize(sys));
-  generateOptions(convertTo, sys, capitalize(sys));
-});
-
-function generateOptions(select, optionValue, optionContent) {
-  const option = document.createElement("option");
-
-  option.value = optionValue;
-  option.textContent = optionContent;
-
-  select.appendChild(option);
-}
-
-function capitalize(text) {
-  return text.slice(0, 1).toUpperCase() + text.slice(1).toLowerCase();
-}
+const numeralSystems = ["decimal", "hexadecimal", "binary", "roman"]
+generateOptions(numeralSystems, convertFrom)
+generateOptions(numeralSystems, convertTo)
 
 function calculate() {
-  let res = "";
+  let res = ""
 
   if (convertFrom.value === "decimal") {
-    res = convertFromDecimal(input.value, convertTo.value);
+    res = convertFromDecimal(input.value, convertTo.value)
   } else if (convertFrom.value === "hexadecimal") {
-    res = convertFromHexadecimal(input.value, convertTo.value);
+    res = convertFromHexadecimal(input.value, convertTo.value)
   } else if (convertFrom.value === "binary") {
-    res = convertFromBinary(input.value, convertTo.value);
+    res = convertFromBinary(input.value, convertTo.value)
   } else if (convertFrom.value === "roman") {
-    res = convertFromRoman(input.value, convertTo.value);
+    res = convertFromRoman(input.value, convertTo.value)
   }
 
-  result.textContent = `Result: ${res}`;
+  result.textContent = `Result: ${res}`
 }
 
-convert.addEventListener("click", calculate);
+convert.addEventListener("click", calculate)
 
 function convertFromDecimal(num, system) {
   if (system === "roman") {
-    return convertToRoman(num);
+    return convertToRoman(num)
   }
   const valuesMap = {
     0: "0",
@@ -63,54 +49,54 @@ function convertFromDecimal(num, system) {
     13: "D",
     14: "E",
     15: "F",
-  };
+  }
   const systemsMap = {
     decimal: 10,
     hexadecimal: 16,
     binary: 2,
-  };
+  }
 
   if (!systemsMap.hasOwnProperty(system)) {
-    return `Invalid numeric system!`;
+    return `Invalid numeric system!`
   }
 
-  let result = "";
+  let result = ""
 
   while (num > 0) {
-    let remainder = num % systemsMap[system];
-    result += valuesMap[remainder];
-    num = Math.floor(num / systemsMap[system]);
+    let remainder = num % systemsMap[system]
+    result += valuesMap[remainder]
+    num = Math.floor(num / systemsMap[system])
   }
 
-  return result.toString().split("").reverse().join("");
+  return result.toString().split("").reverse().join("")
 }
 
 function convertFromBinary(num, system) {
-  num = num.toString();
+  num = num.toString()
 
   if (!num.match(/^[01]+$/g)) {
-    return "Invalid input!";
+    return "Invalid input!"
   }
 
-  let resultToDecimal = 0;
-  let position = 0;
+  let resultToDecimal = 0
+  let position = 0
 
   for (let i = num.length - 1; i >= 0; i--) {
-    let currentDigit = Number(num[i]);
+    let currentDigit = Number(num[i])
     if (currentDigit === 1) {
-      resultToDecimal += currentDigit * 2 ** position;
+      resultToDecimal += currentDigit * 2 ** position
     }
-    position++;
+    position++
   }
 
-  return convertFromDecimal(resultToDecimal, system);
+  return convertFromDecimal(resultToDecimal, system)
 }
 
 function convertFromHexadecimal(num, system) {
-  num = num.toString();
+  num = num.toString()
 
   if (!num.match(/^[0-9A-F]+$/g)) {
-    return "Invalid input!";
+    return "Invalid input!"
   }
 
   const valuesMap = {
@@ -130,22 +116,22 @@ function convertFromHexadecimal(num, system) {
     D: "13",
     E: "14",
     F: "15",
-  };
-
-  let resultToDecimal = 0;
-  let position = 0;
-
-  for (let i = num.length - 1; i >= 0; i--) {
-    let currentChar = num[i];
-    resultToDecimal += valuesMap[currentChar] * 16 ** position;
-    position++;
   }
 
-  return convertFromDecimal(resultToDecimal, system);
+  let resultToDecimal = 0
+  let position = 0
+
+  for (let i = num.length - 1; i >= 0; i--) {
+    let currentChar = num[i]
+    resultToDecimal += valuesMap[currentChar] * 16 ** position
+    position++
+  }
+
+  return convertFromDecimal(resultToDecimal, system)
 }
 
 function convertFromRoman(num, system) {
-  num = num.toString();
+  num = num.toString()
 
   const valuesMap = {
     I: 1,
@@ -155,19 +141,19 @@ function convertFromRoman(num, system) {
     C: 100,
     D: 500,
     M: 1000,
-  };
-
-  let resultToDecimal = 0;
-
-  for (let i = 0; i < num.length; i++) {
-    let romanSymbol = num[i];
-    if (!valuesMap.hasOwnProperty(romanSymbol)) {
-      return "Invalid input!";
-    }
-    resultToDecimal += valuesMap[romanSymbol];
   }
 
-  return convertFromDecimal(resultToDecimal, system);
+  let resultToDecimal = 0
+
+  for (let i = 0; i < num.length; i++) {
+    let romanSymbol = num[i]
+    if (!valuesMap.hasOwnProperty(romanSymbol)) {
+      return "Invalid input!"
+    }
+    resultToDecimal += valuesMap[romanSymbol]
+  }
+
+  return convertFromDecimal(resultToDecimal, system)
 }
 
 function convertToRoman(num) {
@@ -185,18 +171,18 @@ function convertToRoman(num) {
     500: "D",
     900: "CM",
     1000: "M",
-  };
+  }
 
-  let result = "";
+  let result = ""
 
   while (num > 0) {
     let newMap = Object.keys(mapValues)
       .map(Number)
-      .filter((key) => key <= num);
-    let highestValue = Math.max(...newMap);
-    result += mapValues[highestValue];
-    num -= highestValue;
+      .filter((key) => key <= num)
+    let highestValue = Math.max(...newMap)
+    result += mapValues[highestValue]
+    num -= highestValue
   }
 
-  return result;
+  return result
 }
